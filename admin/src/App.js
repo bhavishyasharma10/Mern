2b5a432f8b9e682f5afbfd81602592a762bebd6b
bhavishyasharma10 <bhavishyasharma10@gmail.com>
2021-12-08 01:23:47 +0530

    Added User Data Grid

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,7 +2,8 @@ import Topbar from "./components/Topbar";
 import styled from "styled-components";
 import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserList from "./pages/UserList";
 const Container = styled.div`
   display: flex;
 `;
@@ -12,7 +13,14 @@ function App() {
       <Topbar />
       <Container>
         <Sidebar />
-        <Home />
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+          </Routes>
+          <Routes>
+            <Route exact path="/users" element={<UserList />} />
+          </Routes>
+        </Router>
       </Container>
     </div>
   );
